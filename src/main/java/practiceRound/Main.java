df214a48367ae224ebd05a814d618cd8b6ce12c7
James Lin <jameslin@james.lin>
2021-02-26 14:54:56 +0800

    construct init input.

--- src/main/java/practiceRound/Main.java
+++ src/main/java/practiceRound/Main.java
@@ -3,6 +3,8 @@ package practiceRound;
 import java.io.File;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -22,11 +24,42 @@ public class Main {
 
             File f = new File(m.exampleDir + "/a_example");
             Scanner scanner = new Scanner(f);
-            System.out.println("file : " + scanner.nextInt());
+            int pizzaCount = scanner.nextInt();
+            int[] teams = new int[3]; // each team people : 2, 3, 4; teams[0] mean that there are teams[0] teams with 2 people.
+            for (int tIdx = 0; tIdx < teams.length; tIdx++) {
+                teams[tIdx] = scanner.nextInt();
+                System.out.println((tIdx + 2) + " people team count ");
+            }
+            scanner.nextLine();
+            PizzaIngredient[] pizzas = new PizzaIngredient[pizzaCount];
+            // construct pizza
+            for (int pIdx = 0; pIdx < pizzaCount; pIdx++) {
+                int gradientCount = scanner.nextInt();
+                ArrayList<String> ingredients = new ArrayList<>();
+                for (int gIdx = 0; gIdx < gradientCount; gIdx++) {
+                    ingredients.add(scanner.next());
+                }
+                pizzas[pIdx] = new PizzaIngredient(ingredients, gradientCount);
+                scanner.nextLine();
+            }
+
+            for (int ii = 0; ii < pizzas.length; ii++) {
+                System.out.print(pizzas[ii].ingredientsCount + " ");
+            }
+
 
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+}
+
+class PizzaIngredient {
+    List<String> ingredients;
+    int ingredientsCount = 0;
+    PizzaIngredient(List<String> ingredients, int count) {
+        this.ingredients = ingredients;
+        this.ingredientsCount = count;
     }
 }
